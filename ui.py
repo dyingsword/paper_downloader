56d1ef032817deddf02d5971a289232077d5042b
dyingsword <h1478640753@gmail.com>
2025-04-22 00:01:30 +0800

    实现基本功能，在gui界面中显示论文列表

--- ui.py
+++ ui.py
@@ -4,7 +4,7 @@ import loader as ld
 
 # 创建窗口
 window = tk.Tk()
-window.title("论文下载器")
+window.title("论文列表下载器")
 window.geometry("600x400")
 
 # 定义搜索按钮响应事件
@@ -12,8 +12,9 @@ def search():
     scientist_name = entry.get()
     pid=ld.get_author_pid( scientist_name)
     result=ld.get_author_publications(pid)
+    output_text.delete("1.0", tk.END)
     for item in result:
-        print(f"{item}\n")
+        output_text.insert(tk.END, item + "\n")
     
 
 # 设置整个窗口可伸缩
@@ -31,6 +32,9 @@ entry.pack()
 
 button = tk.Button(window, text="搜索", command=search)
 button.pack(pady=10)
+# 定义文本输出
+output_text = tk.Text(window, height=15, wrap=tk.WORD)
+output_text.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
 
 # 进入主循环
 window.mainloop()
